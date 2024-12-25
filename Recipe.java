2f08d0610447a2d0d61725e947239c3574f56384
tahi20 <167852439+tahi20@users.noreply.github.com>
2024-12-25 09:28:16 +0600

    Update Recipe.java

--- Recipe.java
+++ Recipe.java
@@ -1,43 +1,54 @@
 package Main;
-import org.example.recipe.Category;
-
 public class Recipe {
     private int id;
     private String name;
     private String instructions;
-    private Category category;
-    private String difficulty; 
+    private String category;
+    private String difficulty;
 
-    public Recipe() {
-    }
-
-    public Recipe(String name, String instructions, Category category, String difficulty) {
+    public Recipe(String name, String instructions, String difficulty, String category) {
         this.name = name;
         this.instructions = instructions;
+        this.difficulty = difficulty;
         this.category = category;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInstructions() {
+        return instructions;
+    }
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getDifficulty() {
+        return difficulty;
+    }
+    public void setDifficulty(String difficulty) {
         this.difficulty = difficulty;
     }
+    public Recipe() {
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getInstructions() { return instructions; }
-    public void setInstructions(String instructions) {this.instructions = instructions;}
-    public Category getCategory() { return category; }
-    public void setCategory(Category category) { this.category = category; }
-    public String getDifficulty() { return difficulty; }
-    public void setDifficulty(String difficulty) { this.difficulty = difficulty; }
-
+    }
     @Override
     public String toString() {
-        return "Recipe{" +
-                "id=" + id +
-                ", recipename='" + name + '\'' +
-                ", instructions='" + instructions + '\'' +
-                ", category=" + category + '\''+
-                '}';
+        return
+                 ' ' + id + ' ' + ' ' + name + ' ' +  instructions + ' '+ difficulty + ' ' + category + ' ';
     }
 }
-
 
